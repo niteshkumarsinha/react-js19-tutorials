4239094ddf27f86c9de26a7bcd8485e3df11e4c3
Nitesh Kumar <nitesh111sinha@gmail.com>
2026-01-21 17:58:37 +0530

    Update App.jsx with image and movie content display

--- react-19/src/App.jsx
+++ react-19/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello, Nitesh</h1>
+      <img src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" width="100%" height="100%" />
+
+      <div>
+        <h1>Hello, Nitesh</h1>
+        <h2>Name: Queen of Tears</h2>
+        <h3>Rating: 5</h3>
+        <p>summary of the movie</p>
+      </div>
     </>
   )
 }
