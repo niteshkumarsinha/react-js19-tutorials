a848042838b1a4a6cc7762cd9ca8a8915b970216
Nitesh Kumar <nitesh111sinha@gmail.com>
2026-01-25 14:03:24 +0530

    react components

--- react-19/src/App.jsx
+++ react-19/src/App.jsx
@@ -4,6 +4,17 @@ function App() {
 
   return (
     <>
+     <NetflixSeries /> 
+    </>
+  )
+}
+
+export default App
+
+
+const NetflixSeries = () => {
+  return (
+    <div>
       <img src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" width="100%" height="100%" />
 
       <div>
@@ -11,9 +22,7 @@ function App() {
         <h2>Name: Queen of Tears</h2>
         <h3>Rating: 5</h3>
         <p>summary of the movie</p>
-      </div>
-    </>
+      </div>  
+    </div>
   )
 }
-
-export default App
